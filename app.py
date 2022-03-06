0cdef3bfe58cb085787c067fe0f94dbd166a8168
Ronald <ron_jusuf@yahoo.com>
2022-03-06 14:34:51 +1100

    flashalchemy

--- app.py
+++ app.py
@@ -2,7 +2,19 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:CaitlynClarise0711@localhost/height_collector'
 db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    height = db.Column(db.Integer)
+
+    def __init__(self, email_, height_):
+        self.email_ = email_
+        self.height_ = height_
 
 @app.route('/')
 def home():
